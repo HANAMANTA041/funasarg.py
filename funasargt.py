884831e5e50e68e7697c115e87b564bae7c72738
HANAMANTA <111106091+HANAMANTA041@users.noreply.github.com>
2022-08-19 16:49:38 +0530

    funasarg.md

---
+++ funasargt.py
@@ -0,0 +1,25 @@
+"""
+def trial(x, y):
+    print(x +" "+y)
+
+trial("hello","world")
+list = ['car', 'bus', 'bike', 'scooter']
+def loop(x):
+    print(x*3)
+
+loop(list)
+list = [1, 2, 3, 4, 5]
+def loop(x):
+    print(x*3)
+def map_simple(crazy,list):
+    for items in list:
+        crazy(items)
+
+map_simple(loop,list)"""
+
+def my_function(fname,lname):
+    print(lname+" "+fname)
+
+my_function("Coco","Sneth")
+
+
